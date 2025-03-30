f108ac1c2c2d5d69e5d0d7a3dec5c6f162b5e530
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2025-03-30 22:41:09 +0530

    animate form page change

--- composeApp/src/desktopMain/kotlin/dbms/project/screen/TicketRegistration.kt
+++ composeApp/src/desktopMain/kotlin/dbms/project/screen/TicketRegistration.kt
@@ -1,16 +1,15 @@
 package dbms.project.screen
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import dbms.project.Context
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -23,10 +22,30 @@ fun TicketRegistration(
     context : Context
 ) {
     val dataStore : Array<MutableList<Pair<String,MutableState<String>>>> = remember { Array(listOfScreens.size) { mutableStateListOf() } }
+    var previousIndex by remember { mutableStateOf(0) }
     var activeIndex by remember { mutableStateOf(0) }
     Box( Modifier.fillMaxSize() , contentAlignment = Alignment.Center ) {
-        listOfScreens.forEachIndexed { index, s ->
-            if ( activeIndex == index ) s(context, { activeIndex++ } , { activeIndex-- } , dataStore[index] )
+        listOfScreens.forEachIndexed { index, screen ->
+            AnimatedVisibility( activeIndex == index ,
+                enter = if ( previousIndex < activeIndex ) slideInHorizontally( initialOffsetX = { it * 2 } ) else slideInHorizontally( initialOffsetX = { -it }) ,
+                exit = if ( previousIndex < activeIndex ) slideOutHorizontally( targetOffsetX = { -it }) else slideOutHorizontally( targetOffsetX = { it * 2 }) ,
+            ) {
+                screen(context, {
+                    previousIndex = activeIndex++
+                } , {
+                    previousIndex = activeIndex--
+                } , dataStore[index] )
+            }
+        }
+    }
+    Row ( Modifier.fillMaxSize().padding( 15.dp ) , horizontalArrangement = Arrangement.Center , verticalAlignment = Alignment.Bottom ) {
+        listOfScreens.forEachIndexed { index, _ ->
+            AnimatedVisibility( index == activeIndex ) {
+                Text( "*" , color = MaterialTheme.colors.primary )
+            }
+            AnimatedVisibility( index != activeIndex ) {
+                Text( "*" , color = MaterialTheme.colors.secondary )
+            }
         }
     }
     DisposableEffect(null) {
@@ -45,6 +64,7 @@ fun TicketRegistration(
 }
 
 private val listOfScreens : List<@Composable (context:Context,nextButton:()->Unit,prevButton:()->Unit,dataStore : MutableList<Pair<String,MutableState<String>>>) -> Unit> = listOf(
+    // Train
     { context , nextButton , prevButton , dataStore ->
         generic(context,nextButton,prevButton,dataStore, showPrevButton = false , entries = listOf(
             "Train_code",
@@ -57,19 +77,85 @@ private val listOfScreens : List<@Composable (context:Context,nextButton:()->Uni
             "End_time"
         ), name = "Train Name")
     }
+    // Zone Id
     ,  { context , nextButton , prevButton , dataStore ->
         generic(context,nextButton,prevButton,dataStore, entries = listOf(
             "Zone_id", "Zone_name"
         ) , name = "Zone Id")
-    } ,{ context , nextButton , prevButton , dataStore ->
+    } ,
+    // Station From
+    { context , nextButton , prevButton , dataStore ->
         generic(context,nextButton,prevButton,dataStore, entries = listOf(
             "Station_code", "Station_name"
         ) , name = "Station : From")
-    } ,{ context , nextButton , prevButton , dataStore ->
+    } ,
+    // Station To
+    { context , nextButton , prevButton , dataStore ->
         generic(context,nextButton,prevButton,dataStore, entries = listOf(
             "Station_code", "Station_name"
         ) , name = "Station : To")
     } ,
+    // Class
+    { context , nextButton , prevButton , dataStore ->
+        generic(context,nextButton,prevButton,dataStore, entries = listOf(
+            "Class_id", "Class_code", "Seat_per_coach"
+        ) , name = "Class")
+    } ,
+    // Ticket Reservation
+    { context , nextButton , prevButton , dataStore ->
+        generic(context,nextButton,prevButton,dataStore, entries = listOf(
+            "PNR_no", "Train_code", "From_date", "To_date", "From_Km", "To_Km", "From_Station", "To_Station", "Login_id"
+        ) , name = "Ticket Reservation")
+    } ,
+    // Pax Info
+    { context , nextButton , prevButton , dataStore ->
+        generic(context,nextButton,prevButton,dataStore, entries = listOf(
+            "PAX_name", "PAX_age", "PAX_sex", "Seat_no", "Login_id"
+        ) , name = "Pax Info")
+    } ,
+    // Refund Rule
+    { context , nextButton , prevButton , dataStore ->
+        generic(context,nextButton,prevButton,dataStore, entries = listOf(
+            "From_time", "To_time", "Refundable_amt", "Login_id"
+        ) , name = "Refund Rule")
+    } ,
+    // Pay Info
+    { context , nextButton , prevButton , dataStore ->
+        generic(context,nextButton,prevButton,dataStore, entries = listOf(
+            "Payment_id", "Inst_type", "Inst_amt", "Pay_mode", "Amount", "Pay_date", "PNR_no"
+        ) , name = "Pay Info")
+    } ,
+    // Train Fare
+    { context , nextButton , prevButton , dataStore ->
+        generic(context,nextButton,prevButton,dataStore, entries = listOf(
+            "Train_code", "Class_id", "From_Km", "To_Km", "From_Date", "To_Date", "Fare"
+        ) , name = "Train Fare")
+    } ,
+    // Seat Availability
+    { context , nextButton , prevButton , dataStore ->
+        generic(context,nextButton,prevButton,dataStore, entries = listOf(
+            "Train_code", "Details_id", "No_of_seats"
+        ) , name = "Seat Availability")
+    } ,
+    // Via Details
+    { context , nextButton , prevButton , dataStore ->
+        generic(context,nextButton,prevButton,dataStore, entries = listOf(
+            "Train_code", "Via_station_code", "Via_station_name", "Reach_time", "Km_from_origin"
+        ) , name = "Via Details")
+    } ,
+    // Train Class
+    { context , nextButton , prevButton , dataStore ->
+        generic(context,nextButton,prevButton,dataStore, entries = listOf(
+            "Train_code", "Class_id"
+        ) , name = "Train Class")
+    } ,
+    // Train Station
+    { context , nextButton , prevButton , dataStore ->
+        generic(context,nextButton,prevButton,dataStore, entries = listOf(
+            "Train_code", "Station_code"
+        ) , name = "Train Station")
+    } ,
+    // End Page
     { context , _ , prevButton , _ ->
         Box( Modifier.fillMaxSize() , contentAlignment = Alignment.Center ) {
             Column {
@@ -116,16 +202,17 @@ private fun generic(
             }
         },
         bottomBar = {
-            Row {
+            Row ( modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween ) {
                 if ( showPrevButton ) Button(
                     onClick = prevButton ,
-                    modifier = Modifier.padding( start = 10.dp , bottom = 5.dp)
+                    modifier = Modifier
+                        .padding( start = 10.dp , end = 10.dp , bottom = 5.dp)
                 ) {
                     Text( "Prev" )
                 }
                 if ( showNextButton ) Button(
                     onClick = nextButton ,
-                    modifier = Modifier.padding( start = 10.dp , bottom = 5.dp)
+                    modifier = Modifier.padding( start = 10.dp , end = 10.dp , bottom = 5.dp)
                 ) {
                     Text( "Next" )
                 }
@@ -150,8 +237,8 @@ private fun generic(
     }
 
     LaunchedEffect(null) {
-        println( "Fill Default value of TrainInsert" )
         if ( dataStore.isEmpty() ) {
+            println( "Fill Default value of \"$name\"" )
             entries.forEach {
                 dataStore.add(it to mutableStateOf(""))
             }
